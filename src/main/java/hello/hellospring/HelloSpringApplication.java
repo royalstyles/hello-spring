ff9a4296c2431e976b660a5db42f613fff5ddcaf
Jungho <royalstyles@naver.com>
2022-02-04 12:23:36 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Spring 시작
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
